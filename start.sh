fc135c53e392dee626afc4eb707d07d4bdbf0fe1
Mokshb6458 <115135419+Mokshb6458@users.noreply.github.com>
2022-10-22 16:54:42 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/AM-ROBOTS/AutoAnurag.git /AutoAnurag
+  git clone https://github.com/Mokshb6458/url-auto-delete-shortener-bot /eva
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /AutoAnurag
+  git clone $UPSTREAM_REPO /eva
 fi
-cd /AutoAnurag
+cd /eva
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
