f9d2a785ad64d92897a2a4851f4edb2800de66d8
Greymattersbot <110471652+Greymattersbot@users.noreply.github.com>
2022-10-22 23:46:43 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -6,7 +6,7 @@ else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /Eva
 fi
-cd /eva
+cd /Eva
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
