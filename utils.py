458e2f5308c4111855fd57f3d1dc4fd43e42df75
Mokshb6458 <115135419+Mokshb6458@users.noreply.github.com>
2022-10-22 19:06:04 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -375,3 +375,27 @@ def humanbytes(size):
         size /= power
         n += 1
     return str(round(size, 2)) + " " + Dic_powerN[n] + 'B'
+
+async def get_shortlink(link):
+    https = link.split(":")[0]
+    if "http" == https:
+        https = "https"
+        link = link.replace("http", https)
+    url = f'https://dulink.in/api'
+    params = {'api': SHORTENER_API,
+              'url': link,
+              }
+
+    try:
+        async with aiohttp.ClientSession() as session:
+            async with session.get(url, params=params, raise_for_status=True, ssl=False) as response:
+                data = await response.json()
+                if data["status"] == "success":
+                    return data['shortenedUrl']
+                else:
+                    logger.error(f"Error: {data['message']}")
+                    return f'https://{URL_SHORTENR_WEBSITE}/st?api={SHORTENER_API}&link={link}'
+
+    except Exception as e:
+        logger.error(e)
+        return f'{URL_SHORTENR_WEBSITE}/st?api={SHORTENER_API}&link={link}'
