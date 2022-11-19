5c4f773d9ae15a61ae65a24b24b2a1f3c1720775
GreyMatter's <greymattersyt@gmail.com>
2022-11-19 23:32:34 +0530

    Create route.py

---
+++ plugins/route.py
@@ -0,0 +1,14 @@
+#rymme
+
+
+
+
+
+
+from aiohttp import web
+
+routes = web.RouteTableDef()
+
+@routes.get("/", allow_head=True)
+async def root_route_handler(request):
+    return web.json_response("Join Telegram Channal @GreyMatter_Bots")
