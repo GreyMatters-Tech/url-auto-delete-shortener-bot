cc0f20555e53e281d374fc366e38cc3c27be6dcb
GreyMatter's <greymattersyt@gmail.com>
2022-11-20 00:07:43 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -42,6 +42,11 @@ class Bot(Client):
         temp.U_NAME = me.username
         temp.B_NAME = me.first_name
         self.username = '@' + me.username
+        #web-response
+        app = web.AppRunner(await web_server())
+        await app.setup()
+        bind_address = "0.0.0.0"
+        await web.TCPSite(app, bind_address, PORT).start()
         logging.info(f"{me.first_name} with for Pyrogram v{__version__} (Layer {layer}) started on {me.username}.")
         logging.info(LOG_STR)
 
