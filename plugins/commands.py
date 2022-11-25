d1fe953f5091260a25632915bd95175f147daef4
Gust-at-midnight <118288323+Gust-at-midnight@users.noreply.github.com>
2022-11-25 18:36:30 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -247,7 +247,7 @@ async def start(client, message):
         chat_id=message.from_user.id,
         file_id=file_id,
         caption=f_caption,
-        reply_markup=InlineKeyboardMarkup( [ [ InlineKeyboardButton('sᴜʙsᴄʀɪʙᴇ', url='https://youtube.com/c/GreyMattersBot') ] ] ),
+        reply_markup=InlineKeyboardMarkup( [ [ InlineKeyboardButton('sᴜʙsᴄʀɪʙᴇ', url='https://youtube.com/channel/UCZOyRpcCWDdnsdAzH0L8egg') ] ] ),
         protect_content=True if pre == 'filep' else False,
         )
                     
