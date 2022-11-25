ed17ff0299af66dcffac8035d0b4e7fcf984d36c
Gust-at-midnight <118288323+Gust-at-midnight@users.noreply.github.com>
2022-11-25 13:10:56 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -51,7 +51,7 @@ class script(object):
 3. Buttons should be properly parsed as markdown format
 
 <b>URL buttons:</b>
-<code>[Button Text](buttonurl:https://t.me/GreyMatter_Owner)</code>
+<code>[Button Text](buttonurl:https://t.me/Svaraz)</code>
 
 <b>Alert buttons:</b>
 <code>[Button Text](buttonalert:This is an alert message)</code>"""
