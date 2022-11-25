695e3c8cf553db7ad7fc2172e466fb81deaba9ff
Gust-at-midnight <118288323+Gust-at-midnight@users.noreply.github.com>
2022-11-25 12:13:19 +0530

    Update info.py

--- info.py
+++ info.py
@@ -82,10 +82,10 @@ if SELF_DELETE == "True":
 
     # Download Tutorial Button #
 DOWNLOAD_TEXT_NAME = "📥 HOW TO DOWNLOAD 📥"
-DOWNLOAD_TEXT_URL = "https://t.me/How_to_Download_From_Search_Bot/2"
+DOWNLOAD_TEXT_URL = "https://t.me/+DvF68IyujjIwODJl"
 
    # Custom Caption Under Button #
 CAPTION_BUTTON = "Subscribe"
-CAPTION_BUTTON_URL = "https://youtube.com/c/GreyMattersBot"
+CAPTION_BUTTON_URL = "https://youtube.com/channel/UCZOyRpcCWDdnsdAzH0L8egg"
 
    # Auto Delete For Bot Sending Files #
