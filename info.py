218baa6a368a6d60a0e3e56b3f9864aea920e84f
botac135 <86068776+botac135@users.noreply.github.com>
2022-11-27 09:46:16 +0530

    Update info.py

--- info.py
+++ info.py
@@ -78,7 +78,7 @@ if SELF_DELETE == "True":
     SELF_DELETE = True
 
     # Download Tutorial Button #
-DOWNLOAD_TEXT_NAME = "📥 HOW TO DOWNLOAD 📥"
+DOWNLOAD_TEXT_NAME = "HOW TO DOWNLOAD"
 DOWNLOAD_TEXT_URL = "https://t.me/How_to_Download_From_Search_Bot/2"
 
    # Custom Caption Under Button #
