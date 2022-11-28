ece2aee30be5f4eb3d366373105741af12b12b0a
Race Developer Members <88087547+Shivayt007@users.noreply.github.com>
2022-11-28 13:15:13 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -247,7 +247,7 @@ async def start(client, message):
         chat_id=message.from_user.id,
         file_id=file_id,
         caption=f_caption,
-        reply_markup=InlineKeyboardMarkup( [ [ InlineKeyboardButton('sᴜʙsᴄʀɪʙᴇ', url='https://youtube.com/c/GreyMattersBot') ] ] ),
+        reply_markup=InlineKeyboardMarkup( [ [ InlineKeyboardButton('Updates channel', url='https://telegram.dog/central_links') ] ] ),
         protect_content=True if pre == 'filep' else False,
         )
                     
