a7c2f4ea9ce427cf1a338d9d52cc89ee02e4cd63
botac135 <86068776+botac135@users.noreply.github.com>
2022-11-29 17:17:41 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -113,7 +113,7 @@ async def next_page(bot, query):
 
     btn.insert(0,
         [
-            InlineKeyboardButton(text="🍃𝐻𝑜𝑤 𝑇𝑜 𝐷𝑜𝑤𝑛𝑙𝑜𝑎𝑑?", url='https://t.me/SixFlix/33')
+            InlineKeyboardButton(text="🍃𝙃𝙤𝙬 𝙏𝙤 𝘿𝙤𝙬𝙣𝙡𝙤𝙖𝙙?", url='https://t.me/SixFlix/33')
         ]
     )
 
@@ -125,20 +125,20 @@ async def next_page(bot, query):
         off_set = offset - 10
     if n_offset == 0:
         btn.append(
-            [InlineKeyboardButton("👈𝐵𝑎𝑐𝑘", callback_data=f"next_{req}_{key}_{off_set}"),
+            [InlineKeyboardButton("👈𝘽𝙖𝙘𝙠", callback_data=f"next_{req}_{key}_{off_set}"),
              InlineKeyboardButton(f"📂 𝑃𝑎𝑔𝑒𝑠 {math.ceil(int(offset) / 10) + 1} / {math.ceil(total / 10)}",
                                   callback_data="pages")]
         )
     elif off_set is None:
         btn.append(
             [InlineKeyboardButton(f"📂 {math.ceil(int(offset) / 10) + 1} / {math.ceil(total / 10)}", callback_data="pages"),
-             InlineKeyboardButton("𝑁𝑒𝑥𝑡👉", callback_data=f"next_{req}_{key}_{n_offset}")])
+             InlineKeyboardButton("𝙉𝙚𝙭𝙩👉", callback_data=f"next_{req}_{key}_{n_offset}")])
     else:
         btn.append(
             [
-                InlineKeyboardButton("👈𝐵𝑎𝑐𝑘", callback_data=f"next_{req}_{key}_{off_set}"),
+                InlineKeyboardButton("👈𝘽𝙖𝙘𝙠", callback_data=f"next_{req}_{key}_{off_set}"),
                 InlineKeyboardButton(f"📂 {math.ceil(int(offset) / 10) + 1} / {math.ceil(total / 10)}", callback_data="pages"),
-                InlineKeyboardButton("𝑁𝑒𝑥𝑡👉", callback_data=f"next_{req}_{key}_{n_offset}")
+                InlineKeyboardButton("𝙉𝙚𝙭𝙩👉", callback_data=f"next_{req}_{key}_{n_offset}")
             ],
         )
     try:
@@ -697,7 +697,7 @@ async def auto_filter(client, msg, spoll=False):
 
     btn.insert(0,
         [
-            InlineKeyboardButton(text="🍃𝐻𝑜𝑤 𝑇𝑜 𝐷𝑜𝑤𝑛𝑙𝑜𝑎𝑑?", url='https://t.me/SixFlix/33')
+            InlineKeyboardButton(text="🍃𝙃𝙤𝙬 𝙏𝙤 𝘿𝙤𝙬𝙣𝙡𝙤𝙖𝙙?", url='https://t.me/SixFlix/33')
         ]
     )
 
@@ -707,7 +707,7 @@ async def auto_filter(client, msg, spoll=False):
         req = message.from_user.id if message.from_user else 0
         btn.append(
             [InlineKeyboardButton(text=f"📂 1/{math.ceil(int(total_results) / 10)}", callback_data="pages"),
-             InlineKeyboardButton(text="𝑁𝑒𝑥𝑡👉", callback_data=f"next_{req}_{key}_{offset}")]
+             InlineKeyboardButton(text="𝙉𝙚𝙭𝙩👉", callback_data=f"next_{req}_{key}_{offset}")]
         )
     else:
         btn.append(
