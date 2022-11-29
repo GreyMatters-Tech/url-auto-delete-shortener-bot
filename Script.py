9825b904953eeb22e076ba7573a053bbfc6b9ef4
botac135 <86068776+botac135@users.noreply.github.com>
2022-11-29 11:30:06 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -61,7 +61,7 @@ class script(object):
 3. Buttons should be properly parsed as markdown format
 
 <b>URL buttons:</b>
-<code>[Button Text](buttonurl:https://t.me/GreyMatter_Owner)</code>
+<code>[Button Text](buttonurl:https://t.me/username)</code>
 
 <b>Alert buttons:</b>
 <code>[Button Text](buttonalert:This is an alert message)</code>"""
