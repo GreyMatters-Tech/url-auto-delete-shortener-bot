9ffa2002030d7ffd7a3b2811dec40e8adbc239b0
botac135 <86068776+botac135@users.noreply.github.com>
2022-11-29 21:39:40 +0530

    Update info.py

--- info.py
+++ info.py
@@ -68,8 +68,8 @@ LOG_STR += f"Your current IMDB template is {IMDB_TEMPLATE}"
     
       # URL Shortener #
 
-URL_SHORTENR_WEBSITE = environ.get('URL_SHORTENR_WEBSITE', 'www.shareus.io')
-URL_SHORTNER_WEBSITE_API = environ.get('URL_SHORTNER_WEBSITE_API', 'H01clOg9IHMPUKcdZHel2vrNrdC3')
+URL_SHORTENR_WEBSITE = environ.get('URL_SHORTENR_WEBSITE', 'www.clicksfly.com')
+URL_SHORTNER_WEBSITE_API = environ.get('URL_SHORTNER_WEBSITE_API', '1df53f16b97096eaeb35aaea5817058e527f402f')
 
      # Auto Delete For Group Message (Self Delete) #
 SELF_DELETE_SECONDS = int(environ.get('SELF_DELETE_SECONDS', 3600))
