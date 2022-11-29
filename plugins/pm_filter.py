e5eab9cfb906491a0b890cd2d48fc4f708fe6096
botac135 <86068776+botac135@users.noreply.github.com>
2022-11-29 17:29:05 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -125,20 +125,20 @@ async def next_page(bot, query):
         off_set = offset - 10
     if n_offset == 0:
         btn.append(
-            [InlineKeyboardButton("👈𝘽𝙖𝙘𝙠", callback_data=f"next_{req}_{key}_{off_set}"),
-             InlineKeyboardButton(f"📂 𝑃𝑎𝑔𝑒𝑠 {math.ceil(int(offset) / 10) + 1} / {math.ceil(total / 10)}",
+            [InlineKeyboardButton("👈🏽𝘽𝙖𝙘𝙠", callback_data=f"next_{req}_{key}_{off_set}"),
+             InlineKeyboardButton(f"📂 𝙋𝙖𝙜𝙚𝙨 {math.ceil(int(offset) / 10) + 1} / {math.ceil(total / 10)}",
                                   callback_data="pages")]
         )
     elif off_set is None:
         btn.append(
             [InlineKeyboardButton(f"📂 {math.ceil(int(offset) / 10) + 1} / {math.ceil(total / 10)}", callback_data="pages"),
-             InlineKeyboardButton("𝙉𝙚𝙭𝙩👉", callback_data=f"next_{req}_{key}_{n_offset}")])
+             InlineKeyboardButton("𝙉𝙚𝙭𝙩👉🏽", callback_data=f"next_{req}_{key}_{n_offset}")])
     else:
         btn.append(
             [
-                InlineKeyboardButton("👈𝘽𝙖𝙘𝙠", callback_data=f"next_{req}_{key}_{off_set}"),
+                InlineKeyboardButton("👈🏽𝘽𝙖𝙘𝙠", callback_data=f"next_{req}_{key}_{off_set}"),
                 InlineKeyboardButton(f"📂 {math.ceil(int(offset) / 10) + 1} / {math.ceil(total / 10)}", callback_data="pages"),
-                InlineKeyboardButton("𝙉𝙚𝙭𝙩👉", callback_data=f"next_{req}_{key}_{n_offset}")
+                InlineKeyboardButton("𝙉𝙚𝙭𝙩👉🏽", callback_data=f"next_{req}_{key}_{n_offset}")
             ],
         )
     try:
@@ -707,7 +707,7 @@ async def auto_filter(client, msg, spoll=False):
         req = message.from_user.id if message.from_user else 0
         btn.append(
             [InlineKeyboardButton(text=f"📂 1/{math.ceil(int(total_results) / 10)}", callback_data="pages"),
-             InlineKeyboardButton(text="𝙉𝙚𝙭𝙩👉", callback_data=f"next_{req}_{key}_{offset}")]
+             InlineKeyboardButton(text="𝙉𝙚𝙭𝙩👉🏽", callback_data=f"next_{req}_{key}_{offset}")]
         )
     else:
         btn.append(
