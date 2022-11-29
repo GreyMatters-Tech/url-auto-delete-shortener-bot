3ca53d74a72fff6b46bef85a2a49ba25c1cf7e20
botac135 <86068776+botac135@users.noreply.github.com>
2022-11-29 11:05:03 +0530

    Update info.py

--- info.py
+++ info.py
@@ -72,7 +72,7 @@ URL_SHORTENR_WEBSITE = environ.get('URL_SHORTENR_WEBSITE', 'easysky.in')
 URL_SHORTNER_WEBSITE_API = environ.get('URL_SHORTNER_WEBSITE_API', '371cf03e7e347bebcffdaa24445ea4eec05344d6')
 
      # Auto Delete For Group Message (Self Delete) #
-SELF_DELETE_SECONDS = int(environ.get('SELF_DELETE_SECONDS', 300))
+SELF_DELETE_SECONDS = int(environ.get('SELF_DELETE_SECONDS', 3600))
 SELF_DELETE = environ.get('SELF_DELETE', True)
 if SELF_DELETE == "True":
     SELF_DELETE = True
