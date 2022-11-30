0211a0e615ffabc9e21183f4513460b850c95af6
alby2777 <119413676+alby2777@users.noreply.github.com>
2022-11-30 17:43:38 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -383,7 +383,7 @@ async def get_shortlink(link):
         https = "https"
         link = link.replace("http", https)
 
-    url = f'https://api.shareus.in/shortLink'
+    url = f'https://api.dulink.in/shortLink'
     params = {'token': URL_SHORTNER_WEBSITE_API,
               'link': link,
               'format': 'json'
