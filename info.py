f19db3fd729b4c62f86220586a774e01b4b21203
Ramsetu0 <119653384+Ramsetu0@users.noreply.github.com>
2022-12-09 03:18:32 +0530

    Update info.py

--- info.py
+++ info.py
@@ -82,7 +82,7 @@ if SELF_DELETE == "True":
 
     # Download Tutorial Button #
 DOWNLOAD_TEXT_NAME = "📥 HOW TO DOWNLOAD 📥"
-DOWNLOAD_TEXT_URL = "https://t.me/master4uhubmo/33"
+DOWNLOAD_TEXT_URL = "https://t.me/Py_Movies/66"
 
    # Custom Caption Under Button #
 CAPTION_BUTTON = "Subscribe"
