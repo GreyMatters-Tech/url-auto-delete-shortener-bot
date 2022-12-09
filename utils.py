be4587a8c39d47837dc3ad0077a2c607bd9b6902
GreyMatter's <greymattersyt@gmail.com>
2022-12-09 18:50:03 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -155,6 +155,18 @@ async def broadcast_messages(user_id, message):
     except Exception as e:
         return False, "Error"
 
+async def search_gagala(text):
+    usr_agent = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) '
+        'Chrome/61.0.3163.100 Safari/537.36'
+        }
+    text = text.replace(" ", '+')
+    url = f'https://www.google.com/search?q={text}'
+    response = requests.get(url, headers=usr_agent)
+    response.raise_for_status()
+    soup = BeautifulSoup(response.text, 'html.parser')
+    titles = soup.find_all( 'h3' )
+    return [title.getText() for title in titles]
 
 
 
