fd9f0665f5361b9bec5cb42ea08e823a086e8303
Raman456123 <103272817+Raman456123@users.noreply.github.com>
2022-12-13 20:14:19 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -247,7 +247,7 @@ async def start(client, message):
         chat_id=message.from_user.id,
         file_id=file_id,
         caption=f_caption,
-        reply_markup=InlineKeyboardMarkup( [ [ InlineKeyboardButton('Request movies', url='https://t.me/+XnCNbZ23N5wzZTQ1') ] ] ),
+        reply_markup=InlineKeyboardMarkup( [ [ InlineKeyboardButton('Support Us ❤️', url='https://t.me/Cinemadudes1') ] ] ),
         protect_content=True if pre == 'filep' else False,
         )
                     
