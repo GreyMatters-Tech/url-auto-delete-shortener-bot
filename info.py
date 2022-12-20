0440db53ea7f34d71adaf1d250985c03f68b0beb
Ajaykum01 <115863768+Ajaykum01@users.noreply.github.com>
2022-12-20 21:15:36 +0530

    Update info.py

--- info.py
+++ info.py
@@ -82,7 +82,7 @@ if SELF_DELETE == "True":
 
     # Download Tutorial Button #
 DOWNLOAD_TEXT_NAME = "📥 HOW TO DOWNLOAD 📥"
-DOWNLOAD_TEXT_URL = "https://youtube.com/shorts/_jmx8nMQrZY"
+DOWNLOAD_TEXT_URL = "https://youtube.com/shorts/hQwRY8JKHjQ?feature=share"
 
    # Custom Caption Under Button #
 CAPTION_BUTTON = "Subscribe"
