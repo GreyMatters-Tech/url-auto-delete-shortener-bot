b93218d6cbefa0a801a1cc447c82645069e34eeb
Ayita-P-S <121337793+Ayita-P-S@users.noreply.github.com>
2022-12-24 19:20:38 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -11,7 +11,7 @@ def is_enabled(value, default):
         return default
 
 class script(object):
-    HOME_BUTTONURL_UPDATES = environ.get("HOME_BUTTONURL_UPDATES", 'https://t.me/MX_Networks')
+    HOME_BUTTONURL_UPDATES = environ.get("HOME_BUTTONURL_UPDATES", 'https://clicksfly.com/ref/GreyMatter658')
     START_TXT = environ.get("START_TXT", '''<b>Hello {} 👋 I'm A Bot, I Can Search For Movies and Series 🤩.</b>
 
 <i>Aᴅᴅ ᴍᴇ ᴛᴏ ʏᴏᴜʀ ɢʀᴏᴜᴘ ᴛᴏ sᴇᴇ ᴛʜᴇ ᴍᴀɢɪᴄ ᴏʀ ʀᴇᴀᴅ ᴍᴏʀᴇ ғʀᴏᴍ ᴛʜᴇ ᴍᴇɴᴜ ʙᴇʟᴏᴡ</i>''')
