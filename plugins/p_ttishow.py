b11c4eb121780f1f09aedd28269362a6e17f6536
Ayita-P-S <121337793+Ayita-P-S@users.noreply.github.com>
2022-12-24 23:50:06 +0530

    Update p_ttishow.py

--- plugins/p_ttishow.py
+++ plugins/p_ttishow.py
@@ -75,7 +75,7 @@ async def leave_a_chat(bot, message):
         reply_markup=InlineKeyboardMarkup(buttons)
         await bot.send_message(
             chat_id=chat,
-            text='<b>Hello Friends, \nMy admin has told me to leave from group so i go! If you wanna add me again contact my support group.</b>',
+            text='<b>Hello Friends, \nMy admin has told me to leave from group so i go! If you wanna add me again contact my owner.</b>',
             reply_markup=reply_markup,
         )
 
