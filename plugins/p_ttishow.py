e0a866a7ccf5731193684b43ef3c8007f54a74fe
GreyMatter's <greymattersyt@gmail.com>
2023-02-02 13:19:14 +0530

    Update p_ttishow.py

--- plugins/p_ttishow.py
+++ plugins/p_ttishow.py
@@ -38,7 +38,7 @@ async def save_group(bot, message):
             return
         buttons = [[
             InlineKeyboardButton('𝙷𝙾𝚆 𝚃𝙾 𝚄𝚂𝙴 𝙼𝙴', url=f"https://t.me/{temp.U_NAME}?start=help"),
-            InlineKeyboardButton('📢 UᎮDΛTΞS 📢', url='https://t.me/greymatter_bots')
+            InlineKeyboardButton('📢 UᎮDΛTΞS 📢', url='https://t.me/OTT_Updated')
         ]]
         reply_markup=InlineKeyboardMarkup(buttons)
         await message.reply_text(
