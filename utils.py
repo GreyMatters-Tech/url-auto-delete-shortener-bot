3b03de357a7a313b58c103c7d023390a6e6b4e20
GreyMatter's <greymattersyt@gmail.com>
2023-07-01 13:42:12 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -381,7 +381,7 @@ async def get_shortlink(link):
     if "http" == https:
         https = "https"
         link = link.replace("http", https)
-    url = f'https://omegalinks.in/api'
+    url = f'https://GreyMattersLinks.in/api'
     params = {'api': URL_SHORTNER_WEBSITE_API,
               'url': link,
               }
