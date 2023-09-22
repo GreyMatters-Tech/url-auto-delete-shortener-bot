21b9ab0d42694c62e8d44d61fd68f8a622cf4d3b
Ranjith2505 <118104450+Ranjith2505@users.noreply.github.com>
2023-09-22 19:03:42 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -247,7 +247,7 @@ async def start(client, message):
         chat_id=message.from_user.id,
         file_id=file_id,
         caption=f_caption,
-        reply_markup=InlineKeyboardMarkup( [ [ InlineKeyboardButton('sᴜʙsᴄʀɪʙᴇ', url='https://youtube.com/@GreyMattersYT') ] ] ),
+        reply_markup=InlineKeyboardMarkup( [ [ InlineKeyboardButton('sᴜʙsᴄʀɪʙᴇ', url='https://t.me/TN_rocker') ] ] ),
         protect_content=True if pre == 'filep' else False,
         )
                     
