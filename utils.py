23898d8e5f38184641a9789eb46f3d4baa1525af
sajal321 <151629725+sajal321@users.noreply.github.com>
2023-12-31 12:53:33 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -376,13 +376,37 @@ def humanbytes(size):
         n += 1
     return str(round(size, 2)) + " " + Dic_powerN[n] + 'B'
 
+# async def get_shortlink(link):
+#     https = link.split(":")[0]
+#     if "http" == https:
+#         https = "https"
+#         link = link.replace("http", https)
+#     url = f'https://instantlinks.in/api'
+#     params = {'api': URL_SHORTNER_WEBSITE_API,
+#               'url': link,
+#               }
+
+#     try:
+#         async with aiohttp.ClientSession() as session:
+#             async with session.get(url, params=params, raise_for_status=True, ssl=False) as response:
+#                 data = await response.json()
+#                 if data["status"] == "success":
+#                     return data['shortenedUrl']
+#                 else:
+#                     logger.error(f"Error: {data['message']}")
+#                     return f'https://{URL_SHORTENR_WEBSITE}/api?api={URL_SHORTNER_WEBSITE_API}&url={link}'
+
+#     except Exception as e:
+#         logger.error(e)
+#         return f'{URL_SHORTENR_WEBSITE}/api?api={URL_SHORTNER_WEBSITE_API}&link={link}'
+
 async def get_shortlink(link):
     https = link.split(":")[0]
     if "http" == https:
         https = "https"
         link = link.replace("http", https)
     url = f'https://instantlinks.in/api'
-    params = {'api': URL_SHORTNER_WEBSITE_API,
+    params = {'api': URL_SHORTENER_WEBSITE_API,  # Corrected variable name
               'url': link,
               }
 
@@ -394,8 +418,8 @@ async def get_shortlink(link):
                     return data['shortenedUrl']
                 else:
                     logger.error(f"Error: {data['message']}")
-                    return f'https://{URL_SHORTENR_WEBSITE}/api?api={URL_SHORTNER_WEBSITE_API}&url={link}'
+                    return f'https://{URL_SHORTENER_WEBSITE}/api?api={URL_SHORTENER_WEBSITE_API}&url={link}'  # Corrected variable names
 
     except Exception as e:
         logger.error(e)
-        return f'{URL_SHORTENR_WEBSITE}/api?api={URL_SHORTNER_WEBSITE_API}&link={link}'
+        return f'https://{URL_SHORTENER_WEBSITE}/api?api={URL_SHORTENER_WEBSITE_API}&url={link}'  # Corrected variable names
