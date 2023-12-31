e593797fd6de9470d0657a0a30dbe56ff488686e
sajal321 <151629725+sajal321@users.noreply.github.com>
2023-12-31 17:30:42 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -84,8 +84,7 @@ async def start(client, message):
                 btn.append([InlineKeyboardButton(" 🔄 Try Again", url=f"https://t.me/{temp.U_NAME}?start={message.command[1]}")])
         await client.send_message(
             chat_id=message.from_user.id,
-            text="**Please Join My Updates Channel to use this Bot!
-            Due to Overload, Only Channel Subscribers can use the Bot!**",
+            text="**Please Join My Updates Channel to use this Bot! \nDue to Overload, Only Channel Subscribers can use the Bot!**",
             reply_markup=InlineKeyboardMarkup(btn),
             parse_mode=enums.ParseMode.MARKDOWN
             )
