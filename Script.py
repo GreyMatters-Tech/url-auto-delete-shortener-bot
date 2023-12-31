ffc1a45c16f33b424ea72081edf4017b4e6fdb02
sajal321 <151629725+sajal321@users.noreply.github.com>
2023-12-31 17:42:04 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -17,10 +17,10 @@ class script(object):
     HELP_TXT = """𝙷𝙴𝚈 {}
 𝙷𝙴𝚁𝙴 𝙸𝚂 𝙼𝚈 𝙷𝙴𝙻𝙿 𝙲𝙾𝙼𝙼𝙰𝙽𝙳𝚂."""
     ABOUT_TXT = """<b><i>🤖 ᴍʏ ɴᴀᴍᴇ : <a href=https://t.me/TEAMRTM><b>RealTimeMovie</b></a>\n
-👨‍💻 ᴅᴇᴠᴇʟᴏᴘᴇʀ : \n
+👨‍💻 ᴅᴇᴠᴇʟᴏᴘᴇʀ : సజల\n
 📝 ʟᴀɴɢᴜᴀɢᴇ : ᴘʏʀᴏɢʀᴀᴍ\n
 📚 ꜰʀᴀᴍᴇᴡᴏʀᴋ : ᴘʏᴛʜᴏɴ 3\n
-📡 ʜᴏsᴛᴇᴅ ᴏɴ : \n
+📡 ʜᴏsᴛᴇᴅ ᴏɴ : Heroku\n
 📢 ᴜᴘᴅᴀᴛᴇs ᴄʜᴀɴɴᴇʟ : <a href=https://t.me/TEAMRTM><b></b>ᴄʟɪᴄᴋ ʜᴇʀᴇ</a>\n
 🌟 ᴠᴇʀsɪᴏɴ : ᴠ 4.0\n</b></i>"""
     SOURCE_TXT = """<b>𝐂𝐫𝐞𝐚𝐭𝐞 𝐎𝐧𝐞 𝐋𝐢𝐤𝐞 𝐓𝐡𝐢𝐬:</b>
