d503ba3d566a9d8c85a7bac9138533612bece4e7
sajal321 <151629725+sajal321@users.noreply.github.com>
2024-01-02 02:10:34 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -245,7 +245,7 @@ async def start(client, message):
         chat_id=message.from_user.id,
         file_id=file_id,
         caption=f_caption,
-        reply_markup=InlineKeyboardMarkup( [ [ InlineKeyboardButton('Backup Channel', url='https://t.me/TEAMRTM') ] ] ),
+        # reply_markup=InlineKeyboardMarkup( [ [ InlineKeyboardButton('Backup Channel', url='https://t.me/TEAMRTM') ] ] ),
         protect_content=True if pre == 'filep' else False,
         )
                     
