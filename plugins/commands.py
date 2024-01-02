797b55b8d76423dd06a7f57fb0f40b6a0618d8ca
sajal321 <151629725+sajal321@users.noreply.github.com>
2024-01-03 02:21:38 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -255,8 +255,7 @@ async def start(client, message):
         ),
         protect_content=True if pre == 'filep' else False,
         )
-    reply = fuck.reply_to_messag
-    sex = await fuck.reply("File will be deleted in 10 mins. Save or forward immediately.")
+    sex = await fuck.reply_to_message("File will be deleted in 10 mins. Save or forward immediately.")
     await asyncio.sleep(600)
     await fuck.delete()
     await sex.delete()
