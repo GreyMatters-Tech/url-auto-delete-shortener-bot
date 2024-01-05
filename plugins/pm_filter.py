b2b343b56fba30380260d103aaad1983214e1210
sajal321 <151629725+sajal321@users.noreply.github.com>
2024-01-05 22:13:17 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -772,22 +772,50 @@ async def advantage_spell_chok(msg):
     query = re.sub(
         r"\b(pl(i|e)*?(s|z+|ease|se|ese|(e+)s(e)?)|((send|snd|giv(e)?|gib)(\sme)?)|movie(s)?|new|latest|br((o|u)h?)*|^h(e|a)?(l)*(o)*|mal(ayalam)?|t(h)?amil|file|that|find|und(o)*|kit(t(i|y)?)?o(w)?|thar(u)?(o)*w?|kittum(o)*|aya(k)*(um(o)*)?|full\smovie|any(one)|with\ssubtitle(s)?)",
         "", msg.text, flags=re.IGNORECASE)  # plis contribute some common words
-    
-    
+    query = query.strip() + " movie"
+    g_s = await search_gagala(query)
+    g_s += await search_gagala(msg.text)
+    gs_parsed = []
+    movielist = []
+    gs_parsed = list(dict.fromkeys(gs_parsed))  # removing duplicates https://stackoverflow.com/a/7961425
+    if len(gs_parsed) > 3:
+        gs_parsed = gs_parsed[:3]
+    if gs_parsed:
+        for mov in gs_parsed:
+            imdb_s = await get_poster(mov.strip(), bulk=True)  # searching each keyword in imdb
+            if imdb_s:
+                movielist += [movie.get('title') for movie in imdb_s]
+    movielist += [(re.sub(r'(\-|\(|\)|_)', '', i, flags=re.IGNORECASE)).strip() for i in gs_parsed]
+    movielist = list(dict.fromkeys(movielist))  # removing duplicates
 
     btn = [[
         InlineKeyboardButton(
-            [
-                [
-                    InlineKeyboardButton("🔍Check Your Spelling", url=f'https://google.com/search?q={msg.text} movie')
-                ], [
-                    InlineKeyboardButton('🗓 Check Release Data', url=f'https://google.com/search?q={msg.text} release date')
-                ]    
-            ]
+            [[
+                InlineKeyboardButton("🔍Check Your Spelling", url=f'https://google.com/search?q={msg.text} movie')
+            ], [
+                InlineKeyboardButton('🗓 Check Release Data', url=f'https://google.com/search?q={msg.text} release date')
+            ]]
+        )
+    ]] 
+
+    if not movielist:
+        k = await msg.reply("I couldn't find a movie in my database. Please check the spelling or the release date and try again.",
+                    reply_markup=InlineKeyboardMarkup(btn))
+        await asyncio.sleep(15)
+        await k.delete()
+        return
+    SPELL_CHECK[msg.id] = movielist
+
+    btn = [[
+        InlineKeyboardButton(
+            [[
+                InlineKeyboardButton("🔍Check Your Spelling", url=f'https://google.com/search?q={msg.text} movie')
+            ], [
+                InlineKeyboardButton('🗓 Check Release Data', url=f'https://google.com/search?q={msg.text} release date')
+            ]]
         )
     ]]
 
-    
     tt = await msg.reply("I couldn't find a movie in my database. Please check the spelling or the release date and try again.",
                     reply_markup=InlineKeyboardMarkup(btn))
     await asyncio.sleep(15)
