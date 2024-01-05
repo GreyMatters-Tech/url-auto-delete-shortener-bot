f38f39f6259e9e8cb278c88fb77d38cad7d5a00d
sajal321 <151629725+sajal321@users.noreply.github.com>
2024-01-05 21:46:14 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -92,7 +92,7 @@ async def next_page(bot, query):
             ]
             for file in files
         ]
-            else:
+    else:
         btn  = [
             [
                 InlineKeyboardButton(
