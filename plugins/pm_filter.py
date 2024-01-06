a8198a13f9396cbe61201da3ec8e3f6dbe1a24b6
sajal321 <151629725+sajal321@users.noreply.github.com>
2024-01-06 22:34:19 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -656,9 +656,9 @@ async def auto_filter(client, msg, spoll=False):
                                                 reply_markup=InlineKeyboardMarkup(
                                                         [
                                                             [
-                                                                InlineKeyboardButton("🔍Check Your Spelling", url=f"https://google.com/search?q={search.text}%20movie")
+                                                                InlineKeyboardButton("🔍Check Your Spelling", url=f'https://google.com/search?q={msga}%20movie')
                                                             ],[
-                                                                InlineKeyboardButton('🗓 Check Release Data', url=f"https://google.com/search?q={search.text}%20release%20date")
+                                                                InlineKeyboardButton('🗓 Check Release Data', url=f'https://google.com/search?q={msg}%20release%20date')
                                                             ]
                                                         ]
                                                     )
