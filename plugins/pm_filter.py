8b1eaaa2ed8eb8f11a90fe2ea092a34dc77b37ee
sajal321 <151629725+sajal321@users.noreply.github.com>
2024-01-06 15:49:04 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -766,6 +766,7 @@ async def auto_filter(client, msg, spoll=False):
             await fuk.delete()
 
 async def advantage_spell_chok(msg):
+    search = msg
     query = re.sub(
         r"\b(pl(i|e)*?(s|z+|ease|se|ese|(e+)s(e)?)|((send|snd|giv(e)?|gib)(\sme)?)|movie(s)?|new|latest|br((o|u)h?)*|^h(e|a)?(l)*(o)*|mal(ayalam)?|t(h)?amil|file|that|find|und(o)*|kit(t(i|y)?)?o(w)?|thar(u)?(o)*w?|kittum(o)*|aya(k)*(um(o)*)?|full\smovie|any(one)|with\ssubtitle(s)?)",
         "", msg.text, flags=re.IGNORECASE)  # plis contribute some common words
